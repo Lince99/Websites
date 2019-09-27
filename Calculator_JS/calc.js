d5d41bf8846bf9e3f41b54206ff8efd1bb7269c3
Lince99 <lynxnic99@gmail.com>
2019-09-27 11:41:23 +0200

    Add extreme cases like 0 and NaN to calculatorJS

--- Calculator_JS/calc.js
+++ Calculator_JS/calc.js
@@ -5,8 +5,12 @@ var tot = 0;
 function get_values() {
     val0 = document.getElementById('val0').value;
     val0 = eval(val0);
+    if(isNaN(val0))
+        val0 = 0;
     val1 = document.getElementById('val1').value;
     val1 = eval(val1);
+    if(isNaN(val1))
+        val1 = 0;
 }
 
 function somma() {
@@ -49,19 +53,30 @@ function equazione() {
 
     delta = Math.pow(b,2) - (4*a*c);
     if(delta < 0) {
-        document.getElementById('ris_eq0').innerHTML = "Impossibile calcolare il Delta";
+        document.getElementById('ris_eq0').innerHTML = "Non esistono soluzioni";
         document.getElementById('ris_eq1').innerHTML = "";
     }
     else if(delta == 0){
         ris0 = ((-1)*b+Math.sqrt(delta)) / (2*a);
-        Math.floor(ris0,)
-        document.getElementById('ris_eq0').innerHTML = ris0;
-        document.getElementById('ris_eq1').innerHTML = "";
+        if(isNaN(ris0)) {
+            document.getElementById('ris_eq0').innerHTML = "Input non valido";
+            document.getElementById('ris_eq1').innerHTML = "!";
+        }
+        else {
+            document.getElementById('ris_eq0').innerHTML = ris0.toFixed(2);
+            document.getElementById('ris_eq1').innerHTML = "";
+        }
     }
     else {
         ris0 = ((-1)*b+Math.sqrt(delta)) / (2*a);
         ris1 = ((-1)*b-Math.sqrt(delta)) / (2*a);
-        document.getElementById('ris_eq0').innerHTML = ris0;
-        document.getElementById('ris_eq1').innerHTML = ris1;
+        if(isNaN(ris0) || isNaN(ris1)) {
+            document.getElementById('ris_eq0').innerHTML = "Input non valido";
+            document.getElementById('ris_eq1').innerHTML = "!";
+        }
+        else {
+            document.getElementById('ris_eq0').innerHTML = ris0.toFixed(2);
+            document.getElementById('ris_eq1').innerHTML = ris1.toFixed(2);
+        }
     }
 }
