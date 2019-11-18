e34016b17f3dde5c50dbec67a6b98742beeff408
Lince99 <lynxnic99@gmail.com>
2019-11-18 11:19:27 +0100

    Add insert queries on football exercise

--- Databases/es_squadra_calcio.sql
+++ Databases/es_squadra_calcio.sql
@@ -1,11 +1,11 @@
 -- CREATE DATABASE 191118_calcio;
 -- USE 191118_calcio;
 
-DROP TABLE IF EXISTS 191118_incontro;
-DROP TABLE IF EXISTS 191118_ingaggio;
-DROP TABLE IF EXISTS 191118_arbitro;
-DROP TABLE IF EXISTS 191118_squadra;
-DROP TABLE IF EXISTS 191118_calciatore;
+-- DROP TABLE IF EXISTS 191118_incontro;
+-- DROP TABLE IF EXISTS 191118_ingaggio;
+-- DROP TABLE IF EXISTS 191118_arbitro;
+-- DROP TABLE IF EXISTS 191118_squadra;
+-- DROP TABLE IF EXISTS 191118_calciatore;
 
 CREATE TABLE IF NOT EXISTS 191118_squadra (
     nome CHAR(30) NOT NULL,
@@ -52,3 +52,47 @@ CREATE TABLE IF NOT EXISTS 191118_incontro (
     FOREIGN KEY(arbitro) REFERENCES 191118_arbitro(cod),
     PRIMARY KEY(data)
 );
+
+
+-- Inserimento calciatori
+
+INSERT INTO 191118_calciatore(id, nome, cognome, ruolo)
+    VALUES (1, 'Mario', 'Rossi', 'Centrale');
+INSERT INTO 191118_calciatore(id, nome, cognome, ruolo)
+    VALUES (2, 'Gianluigi', 'Marconi', 'Portiere');
+INSERT INTO 191118_calciatore(id, nome, cognome, ruolo)
+    VALUES (3, 'Bianco', 'Panna', 'Ala destra');
+INSERT INTO 191118_calciatore(id, nome, cognome, ruolo)
+    VALUES (4, 'Calcio', 'Atore', 'Ala dinistra');
+
+
+-- Inserimento arbitri
+
+INSERT INTO 191118_arbitro(cod, nome, cognome)
+    VALUES (1, 'Regia', 'DaVinci');
+
+
+-- Inserimento squadre
+
+INSERT INTO 191118_squadra(nome, citta, sede, colori)
+    VALUES ('Bari Junior', 'Bari', 'via Treviso', 'Arancione Blu Grigio');
+INSERT INTO 191118_squadra(nome, citta, sede, colori)
+    VALUES ('Napoli Senior', 'Napoli', 'via Napoletana 4', 'Rosso Verde');
+
+
+-- Inserimento ingaggi
+
+INSERT INTO 191118_ingaggio(calciatore, squadra, compenso)
+    VALUES (1, 'Bari Junior', 1000);
+INSERT INTO 191118_ingaggio(calciatore, squadra, compenso)
+    VALUES (2, 'Bari Junior', 1500);
+INSERT INTO 191118_ingaggio(calciatore, squadra, compenso)
+    VALUES (3, 'Napoli Senior', 2000);
+INSERT INTO 191118_ingaggio(calciatore, squadra, compenso)
+    VALUES (4, 'Napoli Senior', 2000);
+
+
+-- Inserimento incontri
+
+INSERT INTO 191118_incontro(data, squadraInCasa, squadraOspite, golSqCasa, golSqFuori, arbitro)
+    VALUES ('2019-11-18 06:06:06', 'Bari Junior', 'Napoli Senior', 2, 30, 1);
